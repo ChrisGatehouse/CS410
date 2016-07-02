14940ca80031dc14ef98f75c8d73bafa56432e8f
moinoue <mohammedinoue@gmail.com>
2016-07-01 18:56:41 -0700

    Set up for the start of the project

---
+++ SampleFTPExample/CS410Project/FTPClient.cs
@@ -0,0 +1,27 @@
+﻿//Main Contributor: Mohammed Inoue
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+/*
+ * This object stores, username, password and destination
+ * And other things when they are needed
+*/
+
+namespace CS410Project
+{
+    class FTPClient
+    {
+        public FTPClient(string username, string password, string destination);
+        public ~FTPClient();
+        //For each fucntionality, the FTP client needs a new method
+        //Call the new object that performs that functionality
+        //TODO:list directory, login, logoff etc
+        private string username;
+        private string password;
+        private string destination;
+    }
+}
---
+++ SampleFTPExample/CS410Project/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS410Project
+{
+    public partial class Main_Window : Form
+    {
+        public Main_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+            //TODO: set FTPClient.username to new username
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            //TODO: set FTPClient.password to new password
+        }
+
+        private void Destination_TextChanged(object sender, EventArgs e)
+        {
+            //TODO: set FTPClient.destination to new destination
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            //TODO do stuff when log in is pressed like connect to whatever
+        }
+    }
+}
