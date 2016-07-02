ca5efa6d7d8ca636d8c82bdc5feccee3b39e6e9d
moinoue <mohammedinoue@gmail.com>
2016-07-02 11:32:40 -0700

    Abstracted away FTPWebRequest in case we need to support SFTP

---
+++ CS410Project/Client.cs
@@ -0,0 +1,45 @@
+﻿//Main Contributor: Mohammed Inoue
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+/*
+ * This object stores, username, password and destination
+ * And other things when they are needed
+ * If we need to also incorporate SFTP, this will act as a base class
+*/
+
+namespace CS410Project
+{
+    public abstract class Client
+    {
+        //Default constructor
+        public Client(){}
+        //Copy Constructor
+        public Client(Client toCopy)
+        {
+            this.username = toCopy.username;
+            this.password = toCopy.password;
+            this.destination = toCopy.destination;
+            this.currDirectory = toCopy.currDirectory;
+        }
+        //For each functionality, the FTP client needs a new method
+        //Call the new object that performs that functionality
+        //TODO:list directory, login, logoff etc as virtual functions to add definitions to in FTPClient
+        public abstract bool establishConnection(); //performs the initial/test connection
+        public abstract List<string> getCurrDirectory(); //returns a list with the current directory's files
+
+
+        protected string username;
+        protected string password;
+        //Destination of the FTP server
+        public string destination {get;set;}
+        /*CurrDirectory will function as what gets appended to the destination to
+         *provide the full path of the "destination" this allows us to keep destination 
+         *as the root directory*/
+        public string currDirectory {get;set;}
+    }
+}
--- CS410Project/FTPClient.cs
+++ CS410Project/FTPClient.cs
@@ -1,27 +1,84 @@
 ﻿//Main Contributor: Mohammed Inoue
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Net;
-
-/*
- * This object stores, username, password and destination
- * And other things when they are needed
-*/
 
 namespace CS410Project
 {
-    class FTPClient
+    /*
+     * FTPClient is the base class of Client, this will handle the FTP connection
+     * This is also made preemptively incase we also need to incorporate SFTP 
+     * connections. If we do, there will be a new class made called SFTPClient that 
+     *that will also be derived from Client
+     */
+    class FTPClient : Client
     {
-        public FTPClient(string username, string password, string destination);
-        public ~FTPClient();
-        //For each fucntionality, the FTP client needs a new method
-        //Call the new object that performs that functionality
-        //TODO:list directory, login, logoff etc
-        private string username;
-        private string password;
-        private string destination;
+        public FTPClient(string username, string password, string destination) :base()
+        {
+            this.username    = username;
+            this.password    = password;
+            this.destination = destination;
+            //At the start the current working directory nothing
+            this.currDirectory = "";
+            //Establish initial connection to the FTP
+            establishConnection();
+        }
+        //Copy Constructor
+        public FTPClient(Client toCopy):base (toCopy)
+        {
+            establishConnection();
+        }
+        //Logs on to the FTP, returns true if success, returns false if error
+        public override bool establishConnection()
+        {
+            request = (FtpWebRequest)WebRequest.Create(destination);
+            request.Credentials = new NetworkCredential(username, password);
+            //Request is going to stay alive, until a timeout, or a logout
+            request.KeepAlive = true;
+            try
+            {
+                //Test the connection
+                WebResponse response = request.GetResponse();
+            }
+            catch(WebException err) 
+            {
+                //Problem connecting, output error to console ad return false
+                Console.WriteLine(err.ToString());
+                return false;
+            }
+            //Connection is valid, return true
+            return true;
+        }
+        //This function creates a list of the files/folders in the current directory
+        public override List<string> getCurrDirectory()
+        {
+            List<string> results = new List<string>();
+            request = (FtpWebRequest)WebRequest.Create(destination + currDirectory);
+            try
+            {
+                //Check if the file exist on the server
+                WebResponse testResponse = request.GetResponse();
+            }
+            catch (WebException err)
+            {
+                //Not a valid file, so returning an empty list
+                Console.WriteLine(err.ToString());
+                return results;
+            }
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+
+            while (!reader.EndOfStream)
+            {
+                results.Add(reader.ReadLine());
+            }
+            return results;
+        }
+        //TODO: add definitions to virtual functions in base class
+        private FtpWebRequest request;
+
     }
 }
