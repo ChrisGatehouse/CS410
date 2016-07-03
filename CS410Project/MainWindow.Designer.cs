284e49335b8d0af0f82c88ddd99596024846f529
moinoue <mohammedinoue@gmail.com>
2016-07-02 23:40:38 -0700

    Logging on functionality from GUI added, along with error detection if given destination is not a valid URI for FTP

--- CS410Project/MainWindow.Designer.cs
+++ CS410Project/MainWindow.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.UsernameTextbox = new System.Windows.Forms.TextBox();
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            this.DestinationTextbox = new System.Windows.Forms.TextBox();
+            this.WorkingDirectory = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
@@ -55,41 +56,50 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Login_Click);
             // 
-            // textBox1
+            // UsernameTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(58, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Username";
-            this.textBox1.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            this.UsernameTextbox.Location = new System.Drawing.Point(58, 36);
+            this.UsernameTextbox.Name = "UsernameTextbox";
+            this.UsernameTextbox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextbox.TabIndex = 2;
+            this.UsernameTextbox.Text = "Username";
+            this.UsernameTextbox.TextChanged += new System.EventHandler(this.Username_TextChanged);
             // 
-            // textBox2
+            // PasswordTextbox
             // 
-            this.textBox2.Location = new System.Drawing.Point(59, 62);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Password";
-            this.textBox2.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            this.PasswordTextbox.Location = new System.Drawing.Point(59, 62);
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextbox.TabIndex = 3;
+            this.PasswordTextbox.Text = "Password";
+            this.PasswordTextbox.TextChanged += new System.EventHandler(this.Password_TextChanged);
             // 
-            // textBox3
+            // DestinationTextbox
             // 
-            this.textBox3.Location = new System.Drawing.Point(58, 88);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.Text = "Destination";
-            this.textBox3.TextChanged += new System.EventHandler(this.Destination_TextChanged);
+            this.DestinationTextbox.Location = new System.Drawing.Point(58, 88);
+            this.DestinationTextbox.Name = "DestinationTextbox";
+            this.DestinationTextbox.Size = new System.Drawing.Size(100, 20);
+            this.DestinationTextbox.TabIndex = 4;
+            this.DestinationTextbox.Text = "Destination";
+            this.DestinationTextbox.TextChanged += new System.EventHandler(this.Destination_TextChanged);
+            // 
+            // WorkingDirectory
+            // 
+            this.WorkingDirectory.FormattingEnabled = true;
+            this.WorkingDirectory.Location = new System.Drawing.Point(279, 36);
+            this.WorkingDirectory.Name = "WorkingDirectory";
+            this.WorkingDirectory.Size = new System.Drawing.Size(284, 134);
+            this.WorkingDirectory.TabIndex = 5;
             // 
             // Main_Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 386);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.WorkingDirectory);
+            this.Controls.Add(this.DestinationTextbox);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Controls.Add(this.UsernameTextbox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Main_Window";
@@ -103,9 +113,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox UsernameTextbox;
+        private System.Windows.Forms.TextBox PasswordTextbox;
+        private System.Windows.Forms.TextBox DestinationTextbox;
+        private System.Windows.Forms.ListBox WorkingDirectory;
     }
 }
 
---
+++ CS410Project/MainWindow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace CS410Project
+{
+    public partial class Main_Window : Form
+    {
+        public Main_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+            //When the textbox changes, the username string is updated
+            username = UsernameTextbox.Text;
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            //When the textbox changes, the password string is updated
+            password = PasswordTextbox.Text;
+        }
+
+        private void Destination_TextChanged(object sender, EventArgs e)
+        {
+            //When the textbox changes, the destination string is updated
+            destination = DestinationTextbox.Text;
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            //For now by default we will just log on using FTP
+            if (checkValidURI(destination))
+            {
+                client = new FTPClient(username, password, destination);
+            }
+            else
+            {
+                //Not a valid FTP link, display error message
+                MessageBox.Show("Destination is not a valid FTP", "You fucked up", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool checkValidURI(string target)
+        {
+            Uri uriTest;
+            //Checks to see if provided destination is a valid URI for an FTP
+            bool testResult = Uri.TryCreate(target, UriKind.Absolute, out uriTest) && (uriTest.Scheme == Uri.UriSchemeFtp);
+            return testResult;
+        }
+
+        public Client client;
+        public string username = "";
+        public string password = "";
+        public string destination = "";
+    }
+}
