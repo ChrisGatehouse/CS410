a7e2a988a979e465f4c8c1100f6eb9ed811a0003
moinoue <mohammedinoue@gmail.com>
2016-07-03 23:34:23 -0700

    Created UpdateDirectory method, which uses an algorithm to check the current directory with the FTP server's and adds/removes files if there are descrepencies. This allows for faster traversal between already gone to folders making use of the data structure built.

--- CS410Project/Directory.cs
+++ CS410Project/Directory.cs
@@ -33,8 +33,6 @@ namespace CS410Project
             if(client.establishConnection())
             {
                 List<string> directory = new List<string>();
-                //Store the current Directory of the client
-                string currDir = client.currDirectory;
                 //Grab the list of files in the current directory
                 directory= client.getCurrDirectory();
                 if (directory == null)
@@ -42,14 +40,12 @@ namespace CS410Project
                     Console.WriteLine("ERROR: invalid directory");
                     return;
                 }
-
                 //Hold result of the subdirectory
                 bool result;
                 //Figure out which files are folders/files
                 for (int i = 0;i < directory.Count;i++)
                 {
-                    client.currDirectory += "/" + directory[i] + "/";
-                    result = client.isFile();
+                    result = client.isFile(directory[i]);
                     /*If result is false, then the FTP gave an error
                     *when it was treated like a folder so we know its a file.*/
                     if (!result)
@@ -60,8 +56,6 @@ namespace CS410Project
                     {
                         workingDir.subdirectory.Add(new Folder(directory[i],workingDir));
                     }
-                    //Restore original current directory
-                    client.currDirectory = currDir;
                 }
             }
         }
@@ -82,10 +76,6 @@ namespace CS410Project
         //To a new one, and initializes new folders to be created
         public void changeToDirectory(Client client,string destination)
         {
-            //Save old name incase of problems
-            string oldDirectory = client.currDirectory;
-            //Append new directory name
-            client.currDirectory += "/" + destination + "/";
             File newWorkingDir = workingDir.subdirectory.Find(x => x.name == destination);
 
             if (newWorkingDir == null)
@@ -94,22 +84,91 @@ namespace CS410Project
                 initializeDirectory(client);
             }else
             {
-                if (client.isFile())
+                if (client.isFile(destination))
                 {
                     //Just move the working directory over to the new one
                     workingDir = (Folder)newWorkingDir;
-                    //TODO: add check so it doesn't have to keep rebuilding the structure if it was already built
-                    initializeDirectory(client);
+                    //Append new directory name
+                    client.currDirectory += "/" + destination + "/";
+                    //checks existing structure so it doesn't have to keep rebuilding the structure from scratch if it was already built
+                    updateConsistency(client);
                 }
                 else
                 {
                     //Selected object is not a file
-                    //As of right now, do nothing but just restore old name
+                    //As of right now, do nothing
                     //However we could return something so it decides to download the file or whatever
-                    client.currDirectory = oldDirectory;
                 }
             }
         }
+        /*Update consistency is going to look through current version of its subdirectory
+        * then compares the names of every file on the server's directory with what it has 
+         * saved, If there is something new not added, it will add it, if thing has been removed
+        * it will remove it*/
+        //NOTE: This needs to be more throughly tested once we have upload/delete implemented
+        public void updateConsistency(Client client)
+        {
+            List<string> currConsistency = client.getCurrDirectory();
+            //Sort the two list before performing the algorithm
+            currConsistency.Sort();
+            int i = 0; //marker for currConsistency
+            int j = 0; //marker for workingDir
+
+            //The idea with this algorithm is to traverse both list simultaneously and 
+            //find any disparities
+            while (i < currConsistency.Count && j < workingDir.subdirectory.Count)
+            {
+                if (string.Compare(currConsistency[i],workingDir.subdirectory[j].name) == 0)
+                {
+                    //Item exist in both list, so skip it
+                    i++;
+                    j++;
+                }
+                else if (string.Compare(currConsistency[i], workingDir.subdirectory[j].name) > 0)
+                {
+                    //remove working directory's jth entry
+                    workingDir.subdirectory.RemoveAt(j);
+                    j++;
+                }
+                else if (string.Compare(currConsistency[i], workingDir.subdirectory[j].name) < 0)
+                {
+                    //Add to working directory
+                    if (client.isFile(currConsistency[i]))
+                    {
+                        workingDir.subdirectory.Add(new Folder(currConsistency[i], workingDir));
+                    }
+                    else
+                    {
+                        workingDir.subdirectory.Add(new File(currConsistency[i]));
+                    }
+                    i++;
+                    
+                }
+            }
+            while (j < workingDir.subdirectory.Count)
+            {
+                //remove remaining items
+                //remove working directory's jth entry
+                workingDir.subdirectory.RemoveAt(j);
+                j++;
+            }            
+            while (i < currConsistency.Count)
+            {
+                //Add to working directory
+                if (client.isFile(currConsistency[i]))
+                {
+                    workingDir.subdirectory.Add(new Folder(currConsistency[i], workingDir));
+                }
+                else
+                {
+                    workingDir.subdirectory.Add(new File(currConsistency[i]));
+                }
+                i++;
+            }
+
+
+        }
+
         //This function changes the currDirectory of the Client 
         //To the parent directory, (if it exist)
         public void changeToParentDirectory(Client client)
@@ -125,6 +184,13 @@ namespace CS410Project
             {
                 Console.WriteLine("ERROR: Current Directory has no Parent");
             }
+        }
+
+        //Used to refresh directory in case there was any changes
+        public void refreshDirectory(Client client)
+        {
+            workingDir.subdirectory.Clear();
+            initializeDirectory(client);
         }
 
         //A folder is a type of file that also contains more files 
